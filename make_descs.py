b35eb445db8b065d360612dc57d6bbc98709398c
user <2017250005@gmail.com>
2021-08-21 03:23:22 +0900

    3

--- make_descs.py
+++ make_descs.py
@@ -3,6 +3,9 @@ import sys
 
 check = attend_check.attend_check()
 
+<<<<<<< Updated upstream:make_descs.py
 ex = {'jh':'http://www.a.com/a'}
 
+=======
+>>>>>>> Stashed changes:server/routes/utils/faceDetection/make_descs.py
 check.get_descs(sys.argv[1])

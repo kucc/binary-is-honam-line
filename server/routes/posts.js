6017c9010389e1eed2f2e51116a501c246e46c38
donghyun <dawnme1228@naver.com>
2021-08-21 03:07:35 +0900

    python mount

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { spawn } = require("child_process");
+
 const { Session } = require("../models/Session");
 const { User } = require("../models/User");
 const { upload } = require("./utils/imageUpload");
@@ -13,6 +15,16 @@ const { upload } = require("./utils/imageUpload");
 // 형태로 보내기
 
 router.post("/create", (req, res) => {
+  User.findAll((err, userList) => {
+    if (err) {
+      return res.json({ success: false, err });
+    } else {
+      const pyProg = spawn("python3", [
+        "./utils/faceDetection/make_descs.py",
+        Json.parse(`{${userList}}`),
+      ]);
+    }
+  });
   const post = new Post(req.body);
 });
 
